9c9f8495bc5c85efc7810eadd5282633ac2bc4fc
uche-inyama <depoulet@yahoo.com>
2020-12-07 16:23:33 +0100

    update index action to accept params, a requirement to implement level 2

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,5 +1,24 @@
 class HeatmapController < ApplicationController
-  def index
-    render plain: 'hello'
+  def index 
+    @dimension =  heatmap_params[:dimension]
+    @response = Response.joins(:employee).group(:driver_name).group(@dimension.to_sym).average(:score)
+    drivers = []
+    finalResult = []
+    @response.each do |keys, value|
+      driver_name, location, value = [keys.first, keys.last, value]
+      if !drivers.include? driver_name
+        drivers << driver_name
+        finalResult << { driver: driver_name, scores: {} }
+      end
+      finalResult.last[:scores][location] = value.round(2)
+    end
+    render json: finalResult 
   end
+
+
+  private
+
+    def heatmap_params
+      params.permit(:dimension)
+    end
 end
